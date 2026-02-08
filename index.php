e577a299e19d3f2651aa2c3d2a8620a5c770aa49
Santiago Rico <santiagorico@gmail.com>
2026-02-08 13:17:57 +0100

    V1.4 HORA FIX DEFINITIVO

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ $busqueda = isset($_GET['q']) ? mysqli_real_escape_string($conexion, $_GET['q'])
                     <img src='{$evento['imagen_url']}' alt='{$evento['titulo']}'>
                     <h3>{$evento['titulo']}</h3>
                     <p><strong>Fecha:</strong> {$evento['fecha_evento']}</p>
-                    <p><strong>Hora:</strong> {$evento['hora_evento']}</p>
+                    <p><strong>Hora:</strong> ".date('H:i', strtotime($evento['hora_evento']))."</p>
                     <p><strong>Precio:</strong> ".number_format($evento['precio'],2)."€</p>
                     <a href='detalles_evento.php?id={$evento['id_evento']}' class='btn'>Ver detalles</a>
                 </article>";
